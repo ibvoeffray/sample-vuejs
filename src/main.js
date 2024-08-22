64eb67aa89edcefa5b8b451e66544b0c389cbcae
ibvoeffray <ibvoeffray@gmail.com>
2024-08-22 09:42:55 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/index.css';
+import './assets/print-BBB.PNG';
+
 
 createApp(App).mount('#app')
 document.title = 'CoDiv Marketing'
